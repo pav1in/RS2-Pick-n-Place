8cd05a6cd08140ce99c256c7a86521b4d1b67245
m21gha <megha.martin21@gmail.com>
2025-03-02 20:20:36 +1100

    ur3 control

---
+++ ur3_control/build/CMakeFiles/move_ur3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/move_ur3.dir/src/move_ur3.cpp.o"
+  "CMakeFiles/move_ur3.dir/src/move_ur3.cpp.o.d"
+  "move_ur3"
+  "move_ur3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/move_ur3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
