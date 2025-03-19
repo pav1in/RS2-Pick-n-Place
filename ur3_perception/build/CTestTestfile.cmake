a0ee00fd7b6f6c700cfb388bc8b1f63c1676bfcc
m21gha <megha.martin21@gmail.com>
2025-03-19 19:59:33 +1100

    changes

--- ur3_perception/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/megha/git/RS2/ur3_perception
-# Build directory: /home/megha/git/RS2/ur3_perception/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- ur3_perception/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/megha/git/RS2/ur3_perception/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
