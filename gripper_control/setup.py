d2ca88fd103a9269e541dc26f7645f52f2028315
pav1in <pavlin.rodrigues@student.uts.edu.au>
2025-05-27 19:04:10 +1000

    Final Node

--- gripper_control/setup.py
+++ gripper_control/setup.py
@@ -9,18 +9,20 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name,
+            ['package.xml']),
+        ('share/' + package_name + '/launch',
+            ['launch/gripper_control.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='pav',
     maintainer_email='pavlin.rodrigues@student.uts.edu.au',
-    description='Gripper control node for RG2 fake‐hardware',
+    description='Gripper control node for RG2 fake-hardware',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            # <executable name> = <module path>:<callable>
             'gripper_control = gripper_control.gripper_control:main',
         ],
     },
